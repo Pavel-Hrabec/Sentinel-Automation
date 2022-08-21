9a1a82c67f22e8ce0185758f4bd0ae3e6edcee78
Pavel-Hrabec <hrabecpavel@gmail.com>
2022-08-21 08:14:44 +0200

    export queries script added to repo

---
+++ Export-Queries.ps1
@@ -0,0 +1,15 @@
+#export only the saved search categories specified in the $Categories variable below.
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$WorkSpaceName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroupName
+
+)
+# Only export saved queries from these categories - comma separated
+
+$Categories = ("Hunting Queries")
+
+(Get-AzOperationalInsightsSavedSearch -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkSpaceName).Value.Properties | Where-Object { $Categories -contains $_.Category } |ConvertTo-Json -depth 100
